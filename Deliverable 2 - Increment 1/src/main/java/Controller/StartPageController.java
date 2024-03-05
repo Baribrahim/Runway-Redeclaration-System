c80cc1260db85fc5a5e60b9bc8cb6996364ebadf
Jame <jems1g21@soton.ac.uk>
2024-03-05 12:23:47 +0000

    Login

---
+++ Deliverable 2 - Increment 1/src/main/java/Controller/StartPageController.java
@@ -0,0 +1,59 @@
+package Controller;
+
+import Model.LoginModel;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class StartPageController implements Initializable {
+    public LoginModel loginModel = new LoginModel();
+@FXML
+    private Label fail;
+    @FXML
+    private TextField userIDField;
+    @FXML
+    private PasswordField passwordField;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if(loginModel.Connected()){
+            fail.setText("Connected");
+        }
+        else{
+            fail.setText("Not Connected");
+        }
+    }
+    public void Login (ActionEvent event) {
+        try {
+            if (loginModel.CorrectInfo(userIDField.getText(), passwordField.getText())) {
+                fail.setText("Login Successful");
+            } else {
+                fail.setText("UserId or Password Incorrect");
+            }
+        } catch (SQLException e) {
+            fail.setText("UserId or Password Incorrect");
+            e.printStackTrace();
+        }
+    }
+
+    public void Login(javafx.event.ActionEvent actionEvent) {
+        try {
+            if (loginModel.CorrectInfo(userIDField.getText(), passwordField.getText())) {
+                fail.setText("Login Successful");
+            } else {
+                fail.setText("UserId or Password Incorrect");
+            }
+        } catch (SQLException e) {
+            fail.setText("UserId or Password Incorrect");
+            e.printStackTrace();
+        }
+    }
+}
